970e82c2ec162048ed66448ecd63dd4f46e91af7
austinhuisinga <57517058+austinhuisinga@users.noreply.github.com>
2019-12-15 22:02:32 -0700

    tallyUpDonations complete.

--- index.js
+++ index.js
@@ -244,8 +244,11 @@ function getRunnersByTShirtSize(runners, tShirtSize) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  const totalDonations = runners.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.donation;
+  }, 0);
+  return totalDonations;
 }
 
 /////////////// CLOSURES ///////////////
